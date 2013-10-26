04159dfefbd7927a9c4081c79145b0a8c6bf080d
Ryan Fitzgerald <ryan.fitzgerald@causes.com>
2013-10-25 17:06:03 -0700

    Add action strings for Twitter

--- lib/open_actions/tweet_at_target.rb
+++ lib/open_actions/tweet_at_target.rb
@@ -12,8 +12,29 @@ module OpenActions
     end
 
     copy_strings do
-      noun 'tweet'
       leader_description '<strong>Tweet at someone</strong> to influence them.'
+      action_noun 'tweet'
+      action_unit 'tweet'
+      call_to_action 'Tweet'
+      call_to_action_short 'Tweet'
+      create_heading 'Create a tweet'
+      cta_incentive 'Add your voice'
+      edit_heading 'Edit tweet'
+      feed_label 'TWEET'
+      general_cta 'tweet'
+      impact_verbed 'tweeted'
+      invitation_accept 'Tweet'
+      inviter_message 'Your voice will make a difference. Tweet with us.'
+      not_verbed_this 'did not tweet'
+      short_verbed 'tweeted'
+      specific_cta 'tweet'
+      take_this_action 'tweet'
+      usage 'Get more tweets'
+      verb 'tweet'
+      verbed 'tweeted'
+      verbed_a 'sent a tweet'
+      verbed_this 'sent this tweet'
+      verbing 'sending this tweet'
     end
 
     def take_action!(data)
